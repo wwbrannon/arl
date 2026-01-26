5a6a0cae8be042395c413003cd30273db0e97a88
William Brannon <will.brannon@gmail.com>
2026-01-26 00:17:09 -0500

    make stdlib functions accessible from R code via :::

--- R/stdlib.R
+++ R/stdlib.R
@@ -14,102 +14,128 @@ rye_load_stdlib <- function(env = NULL) {
     env <- new.env(parent = baseenv())
   }
   # Define basic list functions
-  env$car <- function(lst) {
-    if (is.call(lst) && length(lst) > 0) {
-      lst[[1]]
-    } else if (is.list(lst) && length(lst) > 0) {
-      lst[[1]]
-    } else {
-      NULL
-    }
-  }
-
-  env$cdr <- function(lst) {
-    if (is.call(lst) && length(lst) > 1) {
-      as.call(as.list(lst)[-1])
-    } else if (is.list(lst) && length(lst) > 1) {
-      lst[-1]
-    } else {
-      list()
-    }
-  }
-
-  env$cons <- function(item, lst) {
-    if (is.call(lst)) {
-      as.call(c(list(item), as.list(lst)))
-    } else if (is.list(lst)) {
-      c(list(item), lst)
-    } else {
-      list(item, lst)
-    }
-  }
+  env$car <- rye_stdlib_car
+  env$cdr <- rye_stdlib_cdr
+  env$cons <- rye_stdlib_cons
 
   # Higher-order functions
-  env$map <- function(fn, lst) {
-    if (is.call(lst)) {
-      lst <- as.list(lst)
-    }
-    lapply(lst, fn)
-  }
-
-  env$filter <- function(pred, lst) {
-    if (is.call(lst)) {
-      lst <- as.list(lst)
-    }
-    Filter(pred, lst)
-  }
-
-  env$reduce <- function(fn, lst, init = NULL) {
-    if (is.call(lst)) {
-      lst <- as.list(lst)
-    }
-    if (is.null(init)) {
-      Reduce(fn, lst)
-    } else {
-      Reduce(fn, lst, init = init)
-    }
-  }
+  env$map <- rye_stdlib_map
+  env$filter <- rye_stdlib_filter
+  env$reduce <- rye_stdlib_reduce
 
   # List predicates
-  env$`list?` <- function(x) {
-    is.list(x) || is.call(x)
-  }
-
-  env$`null?` <- function(x) {
-    is.null(x) || (is.list(x) && length(x) == 0) || (is.call(x) && length(x) == 0)
-  }
-
-  env$`symbol?` <- function(x) {
-    is.symbol(x)
-  }
-
-  env$`number?` <- function(x) {
-    is.numeric(x)
-  }
-
-  env$`string?` <- function(x) {
-    is.character(x)
-  }
+  env$`list?` <- rye_stdlib_list_p
+  env$`null?` <- rye_stdlib_null_p
+  env$`symbol?` <- rye_stdlib_symbol_p
+  env$`number?` <- rye_stdlib_number_p
+  env$`string?` <- rye_stdlib_string_p
 
   # Boolean operations
-  env$not <- function(x) {
-    !x
-  }
+  env$not <- rye_stdlib_not
 
   # Arithmetic operators
   # Provide single-character aliases for R operators
-  env$`%` <- base::`%%`  # modulo
+  env$`%` <- rye_stdlib_modulo
 
   # Lisp-style equality (can override base::== if needed)
-  env$`=` <- base::`==`
+  env$`=` <- rye_stdlib_equal
 
   # Output
-  env$display <- function(x) {
-    cat(as.character(x), "\n")
-  }
+  env$display <- rye_stdlib_display
 
   # Return the environment
   # All R base functions (+, -, *, /, <, >, print, etc.) are automatically
   # available via the parent environment chain
   env
+}
+
+rye_stdlib_car <- function(lst) {
+  if (is.call(lst) && length(lst) > 0) {
+    lst[[1]]
+  } else if (is.list(lst) && length(lst) > 0) {
+    lst[[1]]
+  } else {
+    NULL
+  }
+}
+
+rye_stdlib_cdr <- function(lst) {
+  if (is.call(lst) && length(lst) > 1) {
+    as.call(as.list(lst)[-1])
+  } else if (is.list(lst) && length(lst) > 1) {
+    lst[-1]
+  } else {
+    list()
+  }
+}
+
+rye_stdlib_cons <- function(item, lst) {
+  if (is.call(lst)) {
+    as.call(c(list(item), as.list(lst)))
+  } else if (is.list(lst)) {
+    c(list(item), lst)
+  } else {
+    list(item, lst)
+  }
+}
+
+rye_stdlib_map <- function(fn, lst) {
+  if (is.call(lst)) {
+    lst <- as.list(lst)
+  }
+  lapply(lst, fn)
+}
+
+rye_stdlib_filter <- function(pred, lst) {
+  if (is.call(lst)) {
+    lst <- as.list(lst)
+  }
+  Filter(pred, lst)
+}
+
+rye_stdlib_reduce <- function(fn, lst, init = NULL) {
+  if (is.call(lst)) {
+    lst <- as.list(lst)
+  }
+  if (is.null(init)) {
+    Reduce(fn, lst)
+  } else {
+    Reduce(fn, lst, init = init)
+  }
+}
+
+rye_stdlib_list_p <- function(x) {
+  is.list(x) || is.call(x)
+}
+
+rye_stdlib_null_p <- function(x) {
+  is.null(x) || (is.list(x) && length(x) == 0) || (is.call(x) && length(x) == 0)
+}
+
+rye_stdlib_symbol_p <- function(x) {
+  is.symbol(x)
+}
+
+rye_stdlib_number_p <- function(x) {
+  is.numeric(x)
+}
+
+rye_stdlib_string_p <- function(x) {
+  is.character(x)
+}
+
+rye_stdlib_not <- function(x) {
+  !x
+}
+
+rye_stdlib_modulo <- function(x, y) {
+  base::`%%`(x, y)
+}
+
+rye_stdlib_equal <- function(x, y) {
+  base::`==`(x, y)
+}
+
+rye_stdlib_display <- function(x) {
+  cat(as.character(x), "\n")
 }
