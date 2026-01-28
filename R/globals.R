226e15f1117fcce0cd594d670d0bc3cab95f65b7
William Brannon <will.brannon@gmail.com>
2026-01-27 23:43:58 -0500

    appease R CMD check about warning

---
+++ R/globals.R
@@ -0,0 +1,7 @@
+# This is to silence a stupid warning from R CMD check related to promise
+# construction: there are dynamically evaluated variables that aren't lexically
+# in scope when delayedAssign is called but will be when the promise it returns
+# is forced. (see rye_promise_new in R/utils.R)
+if (getRversion() >= "2.15.1") utils::globalVariables(c(
+  ".rye_promise_eval", ".rye_promise_expr", ".rye_promise_env"
+))
