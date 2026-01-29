12b76b78d0dae08bc284fcada76d9ce1d3829600
William Brannon <will.brannon@gmail.com>
2026-01-29 00:35:51 -0500

    add loop/recur macros as in clojure
    Add loop/recur macros to simulate optimized tail recursion despite
    R's lack of built-in support for it, plus docs and tests.

--- tests/testthat/test-stdlib.R
+++ tests/testthat/test-stdlib.R
@@ -369,6 +369,50 @@ test_that("until macro repeats until test is truthy", {
   expect_equal(result, 3)
 })
 
+test_that("loop/recur iterates with rebinding", {
+  env <- new.env(parent = baseenv())
+  rye_load_stdlib(env)
+  import_stdlib_modules(env, c("looping"))
+
+  result <- rye_eval(
+    rye_read("(loop ((i 0) (acc 0)) (if (< i 5) (recur (+ i 1) (+ acc i)) acc))")[[1]],
+    env
+  )
+  expect_equal(result, 10)
+
+  result <- rye_eval(
+    rye_read("(loop ((x 1)) (+ x 2))")[[1]],
+    env
+  )
+  expect_equal(result, 3)
+
+  result <- rye_eval(
+    rye_read("(loop ((i 0) (sum 0)) (if (< i 3) (recur (+ i 1) (+ sum (loop ((j 0) (acc 0)) (if (< j 2) (recur (+ j 1) (+ acc 1)) acc)))) sum))")[[1]],
+    env
+  )
+  expect_equal(result, 6)
+
+  result <- rye_eval(
+    rye_read("(loop ((n 5) (acc 1)) (if (< n 2) acc (recur (- n 1) (* acc n))))")[[1]],
+    env
+  )
+  expect_equal(result, 120)
+
+  result <- rye_eval(
+    rye_read("(loop ((xs (list 1 2 3)) (sum 0)) (if (null? xs) sum (recur (cdr xs) (+ sum (car xs)))))")[[1]],
+    env
+  )
+  expect_equal(result, 6)
+})
+
+test_that("recur errors outside loop", {
+  env <- new.env(parent = baseenv())
+  rye_load_stdlib(env)
+  import_stdlib_modules(env, c("looping"))
+
+  expect_error(rye_eval(rye_read("(recur 1)")[[1]], env), "recur can only be used inside loop")
+})
+
 test_that("not function works", {
   env <- new.env()
   rye_load_stdlib(env)
