f2863eab90dd24f72757cd0e2fc67f141f4215c6
William Brannon <will.brannon@gmail.com>
2026-02-02 14:01:26 -0800

    clean up repl intro message

--- R/repl.R
+++ R/repl.R
@@ -230,12 +230,13 @@ RyeREPL <- R6::R6Class(
       private$require_engine()
 
       self$output_fn(paste0("Rye REPL ", rye_version()), "\n", sep = "")
-      self$output_fn("Type (quit) or press Ctrl+C to exit\n")
       self$output_fn("This is free software; see the source for copying conditions.\n")
       self$output_fn("There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n")
+      self$output_fn("\n")
+      self$output_fn("Type '(quit)' w/o quotes or press Ctrl+C to exit\n")
       self$output_fn(
-        "Builtin readline support:",
-        ifelse(isTRUE(capabilities("readline")), "yes", "no"),
+        "Builtin readline support: ",
+        ifelse(isTRUE(capabilities("readline")), "yes", "no (try rlwrap)"),
         "\n\n",
         sep = ""
       )
